f4a9759fed93d748434e092a19a60238a2632dde
NA <n-aoki@192.168.0.100>
2017-08-26 17:41:03 +0900

    テストコード追加

--- src/test/java/ui/UiUtilsTest.java
+++ src/test/java/ui/UiUtilsTest.java
@@ -9,4 +9,9 @@ public class UiUtilsTest {
 	public void isSmallLength_nameがnull_trueを返すこと() {
 		Assert.assertTrue(UiUtils.isSmallLength(null, "fieldName", 0));
 	}
+
+	@Test
+	public void isSmallLength_nameがnullでない_nameの文字数がlengthより大きい() {
+		Assert.assertFalse(UiUtils.isSmallLength("testName", "fieldName", 3));
+	}
 }
